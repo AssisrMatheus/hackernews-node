0b8ba748625d2c88ae5af95182f699676bafdba1
Matheus Assis <assisr.matheus@gmail.com>
2020-02-15 13:53:41 -0300

    A Simple Query

--- src/index.ts
+++ src/index.ts
@@ -1,16 +1,36 @@
 import { GraphQLServer } from 'graphql-yoga';
 
-// 1
+const links = [
+  {
+    id: 'link-0',
+    url: 'www.howtographql.com',
+    description: 'Fullstack tutorial for GraphQL'
+  }
+];
+
 const typeDefs = `
 type Query {
   info: String!
+  feed: [Link!]!
+}
+
+type Link {
+  id: ID!
+  description: String!
+  url: String!
 }
 `;
 
 // 2
 const resolvers = {
   Query: {
-    info: () => `This is the API of a Hackernews Clone`
+    info: () => `This is the API of a Hackernews Clone`,
+    feed: () => links
+  },
+  Link: {
+    id: (parent: any) => parent.id,
+    description: (parent: any) => parent.description,
+    url: (parent: any) => parent.url
   }
 };
 
