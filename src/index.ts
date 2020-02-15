40c5aa92b234fe0fba1a6b8dfbf2d8e6a14c2459
Matheus Assis <assisr.matheus@gmail.com>
2020-02-15 15:07:14 -0300

    Connecting Server and Database with the Prisma Client

--- src/index.ts
+++ src/index.ts
@@ -1,16 +1,44 @@
+import { GraphQLServer } from 'graphql-yoga';
 import { prisma } from './generated/prisma-client';
 
-async function main() {
-  // Create a new link
-  const newLink = await prisma.createLink({
-    url: 'www.prisma.io',
-    description: 'Prisma replaces traditional ORMs'
-  });
-  console.log(`Created new link: ${newLink.url} (ID: ${newLink.id})`);
+type ContextType = {
+  prisma: typeof prisma;
+};
 
-  // Read all links from the database and print them to the console
-  const allLinks = await prisma.links();
-  console.log(allLinks);
-}
+const resolvers = {
+  Query: {
+    info: () => `This is the API of a Hackernews Clone`,
+    feed: (_root, _args, context: ContextType) => {
+      return context.prisma.links();
+    }
+  },
+  Mutation: {
+    post: (_root, args, context: ContextType) => {
+      return context.prisma.createLink({
+        url: args.url,
+        description: args.description
+      });
+    },
+    updateLink: (_root, args, context: ContextType) => {
+      return context.prisma.updateLink({
+        data: {
+          ...args,
+          id: undefined
+        },
+        where: {
+          id: args.id
+        }
+      });
+    },
+    deleteLink: (_root, args, context: ContextType) => {
+      return context.prisma.deleteLink({ ...args });
+    }
+  }
+};
 
-main().catch(e => console.error(e));
+const server = new GraphQLServer({
+  typeDefs: './src/schema.graphql',
+  resolvers,
+  context: { prisma }
+});
+server.start(() => console.log(`Server is running on http://localhost:4000`));
