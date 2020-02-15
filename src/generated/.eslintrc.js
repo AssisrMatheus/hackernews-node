8d682f2d004948e1e42ca7ac3bfc3de5ee9b6adc
Matheus Assis <assisr.matheus@gmail.com>
2020-02-15 14:55:09 -0300

    Adding a Database

---
+++ src/generated/.eslintrc.js
@@ -0,0 +1,12 @@
+module.exports = {
+  rules: {
+    'jsdoc/require-jsdoc': 'off',
+    'jsdoc/require-description': 'off',
+    'import/no-default-export': 'off',
+    '@typescript-eslint/no-explicit-any': 'off',
+    '@typescript-eslint/camelcase': 'off',
+    '@typescript-eslint/ban-types': 'off',
+    '@typescript-eslint/no-empty-interface': 'off',
+    'prettier/prettier': 'off'
+  }
+};
--- src/index.ts
+++ src/index.ts
@@ -1,48 +1,16 @@
-import { GraphQLServer } from 'graphql-yoga';
+import { prisma } from './generated/prisma-client';
 
-let links = [
-  {
-    id: 'link-0',
-    url: 'www.howtographql.com',
-    description: 'Fullstack tutorial for GraphQL'
-  }
-];
+async function main() {
+  // Create a new link
+  const newLink = await prisma.createLink({
+    url: 'www.prisma.io',
+    description: 'Prisma replaces traditional ORMs'
+  });
+  console.log(`Created new link: ${newLink.url} (ID: ${newLink.id})`);
 
-let idCount = links.length;
+  // Read all links from the database and print them to the console
+  const allLinks = await prisma.links();
+  console.log(allLinks);
+}
 
-const resolvers = {
-  Query: {
-    info: () => `This is the API of a Hackernews Clone`,
-    feed: () => links
-  },
-  Mutation: {
-    post: (parent: any, args: any) => {
-      const link = {
-        id: `link-${idCount++}`,
-        description: args.description,
-        url: args.url
-      };
-      links.push(link);
-      return link;
-    },
-    updateLink: (parent: any, args: any) => {
-      const index = links.findIndex(x => x.id === args.id);
-      links[index] = {
-        ...links[index],
-        ...args
-      };
-      return links[index];
-    },
-    deleteLink: (parent: any, args: any) => {
-      const link = links.find(x => x.id === args.id);
-      links = links.filter(x => x.id !== args.id);
-      return link;
-    }
-  }
-};
-
-const server = new GraphQLServer({
-  typeDefs: './src/schema.graphql',
-  resolvers
-});
-server.start(() => console.log(`Server is running on http://localhost:4000`));
+main().catch(e => console.error(e));
